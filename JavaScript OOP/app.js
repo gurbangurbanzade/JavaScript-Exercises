c2eb72efc5b184591a64d19baaa5ff57d00f3e43
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-05-18 21:43:06 +0400

    Suallarin göstərilməsi

--- JavaScript OOP/app.js
+++ JavaScript OOP/app.js
@@ -40,11 +40,26 @@ Quiz.prototype.sualVer = function () {
 const quiz = new Quiz(suallar);
 
 document.querySelector(".btn-start").addEventListener("click", function () {
-  if (quiz.sualIndex !== quiz.suallar.length) {
-    document.querySelector(".quiz-box").classList.add("active");
-    console.log(quiz.sualVer());
-    quiz.sualIndex++;
+  document.querySelector(".quiz-box").classList.add("active");
+  sualGoster(quiz.sualVer());
+});
+document.querySelector(".next-btn").addEventListener("click", function () {
+  if (quiz.sualIndex + 1 !== quiz.suallar.length) {
+    quiz.sualIndex += 1;
+    sualGoster(quiz.sualVer());
   } else {
     console.log("sual bitdi");
   }
 });
+function sualGoster(sual) {
+  let question = `<span>${sual.sualMetni}</span>`;
+  let options = "";
+  for (let cavab in sual.cavabVariantlari) {
+    options += `
+  <div class="option">
+    <span><b>${cavab}</b>: ${sual.cavabVariantlari[cavab]}</span>
+  </div>`;
+  }
+  document.querySelector(".question-text").innerHTML = question;
+  document.querySelector(".option-list").innerHTML = options;
+}
