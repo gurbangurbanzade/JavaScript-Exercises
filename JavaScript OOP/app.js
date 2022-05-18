945edd3325df2dd771ab4a4c5963fff009557d3c
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-05-18 22:46:32 +0400

    Cavab Variantlarının Seçilməsi

--- JavaScript OOP/app.js
+++ JavaScript OOP/app.js
@@ -42,15 +42,20 @@ const quiz = new Quiz(suallar);
 document.querySelector(".btn-start").addEventListener("click", function () {
   document.querySelector(".quiz-box").classList.add("active");
   sualGoster(quiz.sualVer());
+  document.querySelector(".next-btn").classList.remove("show");
 });
 document.querySelector(".next-btn").addEventListener("click", function () {
   if (quiz.sualIndex + 1 !== quiz.suallar.length) {
     quiz.sualIndex += 1;
     sualGoster(quiz.sualVer());
+    document.querySelector(".next-btn").classList.remove("show");
   } else {
     console.log("sual bitdi");
   }
 });
+const option_list = document.querySelector(".option-list");
+const correctIcon = `<div class="icon"><i class="fas fa-check"></i></div>`;
+const incorrectIcon = `<div class="icon"><i class="fas fa-times"></i></div>`;
 function sualGoster(sual) {
   let question = `<span>${sual.sualMetni}</span>`;
   let options = "";
@@ -60,6 +65,31 @@ function sualGoster(sual) {
     <span><b>${cavab}</b>: ${sual.cavabVariantlari[cavab]}</span>
   </div>`;
   }
+
+  //const option_list = document.querySelector(".option-list");
   document.querySelector(".question-text").innerHTML = question;
-  document.querySelector(".option-list").innerHTML = options;
+  option_list.innerHTML = options;
+
+  const option = option_list.querySelectorAll(".option");
+
+  for (let opt of option) {
+    opt.setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+function optionSelected(option) {
+  let cavab = option.querySelector("span b").textContent;
+  let sual = quiz.sualVer();
+  console.log(option);
+  if (sual.cavbiYoxla(cavab)) {
+    option.classList.add("correct");
+    option.insertAdjacentHTML("beforeend", correctIcon);
+  } else {
+    option.classList.add("incorrect");
+    option.insertAdjacentHTML("beforeend", incorrectIcon);
+  }
+  for (let i = 0; i < option_list.children.length; i++) {
+    option_list.children[i].classList.add("disabled");
+  }
+  document.querySelector(".next-btn").classList.add("show");
 }
