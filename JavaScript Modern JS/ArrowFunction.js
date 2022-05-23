036106919738155ea2df13832b11874ffda9f58c
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-05-23 13:55:17 +0400

    Arrow Function Example

---
+++ JavaScript Modern JS/ArrowFunction.js
@@ -0,0 +1,14 @@
+//ES5
+let salamES5 = function (ad) {
+  console.log("salam " + ad);
+};
+salamES5("Qurban");
+//ES6
+// let salamES6 = () => console.log("saalamES6");
+let salamES6 = (ad) => {
+  console.log("saalamES6 " + ad);
+};
+salamES6("Eli");
+
+let cem = (a, b) => console.log(a + b);
+cem(2, 3);
