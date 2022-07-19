32a408fc2d99a78d9dad60b9785ac228532d3a1d
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-07-19 22:20:09 +0400

    Api vasitesile
    cagirilan olkelerin qonsu olkelerinin tapilmasi

---
+++ 01-Projects/01-AvtomatikInput/app.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  var x = document.getElementById("fname");
+  if (x.value.length == 1) {
+    x.value = x.value[0];
+  } else if (x.value.length == 2) {
+    // x.value = x.value[0] + x.value[1];
+  } else if (x.value.length == 3 && x.value[3]) {
+    x.value = x.value[0] + x.value[1] + "/";
+  } else if (x.value.length == 4 && x.value.length < 5) {
+    x.value = x.value[0] + x.value[1] + "/" + x.value[2];
+  } else if (x.value.length == 5 && x.value.length < 6) {
+    x.value = x.value[0] + x.value[1] + "/" + x.value[3] + x.value[4];
+  } else if (x.value.length >= 6) {
+    x.value = x.value[0] + x.value[1] + "/" + x.value[3] + x.value[4];
+  }
+
+  // console.log(typeof x.value);
+}
--- JavaScript Asynchronous and APIs/callback.js
+++ JavaScript Asynchronous and APIs/callback.js
@@ -1,21 +1,21 @@
 document.querySelector("#btnSearch").addEventListener("click", () => {
   let text = document.querySelector("#txtSearch").value;
   olkeYaz(text);
+  qonsuOlkeleriTap(text);
 });
 
+//
+// Olkenin ozu haqqinda melumatlar
+//
 function olkeYaz(olkeAdi) {
   const request = new XMLHttpRequest();
   request.open("get", `https://restcountries.com/v2/name/` + olkeAdi);
   request.send();
-  console.log(olkeAdi);
-  //
-  // Olkenin ozu haqqinda melumatlar
-  //
+  // console.log(olkeAdi);
   request.addEventListener("load", function () {
     const olkeAxtarilan = JSON.parse(this.responseText);
-    console.log(olkeAxtarilan[0]);
+    // console.log(olkeAxtarilan[0].borders);
     let html = `
-          
     <div class="card-body">
       <div class="row">
         <div class="col-4">
@@ -43,56 +43,59 @@ function olkeYaz(olkeAdi) {
     `;
     document.querySelector("#countryDetail").innerHTML = html;
   });
-
+}
+//
+// Qonsu olkeler haqqinda melumatlar
+//
+function qonsuOlkeleriTap(olkeAdi) {
+  const request = new XMLHttpRequest();
+  request.open("get", `https://restcountries.com/v2/name/` + olkeAdi);
+  request.send();
   request.addEventListener("load", function () {
-    const data = JSON.parse(this.responseText);
-    console.log(data[0]);
+    const olkeAxtarilan = JSON.parse(this.responseText);
+    for (let olke of olkeAxtarilan[0].borders) {
+      console.log(olke);
+      const request = new XMLHttpRequest();
+      request.open("get", `https://restcountries.com/v3.1/alpha?codes=` + olke);
+      request.send();
+      request.addEventListener("load", function () {
+        const olkeAxtarilan = JSON.parse(this.responseText);
+        console.log(olkeAxtarilan[0]);
 
-    // qonsular
-    // const qonsuOlkeler = data[0].borders.toString();
-    // console.log(qonsuOlkeler);
-    // const req = new XMLHttpRequest();
-    // req.open(
-    //   "get",
-    //   `https://restcountries.com/v3.1/alpha?codes=` + qonsuOlkeler
-    // );
-    // req.send();
-    // req.addEventListener("load", function () {
-    //   const data = JSON.parse(this.responseText);
-    //   olke(data[0]);
-    //   console.log(data[0]);
-    // });
+        // console.log(olkeAxtarilan[0].name.common);
+        let html = `
+        <div class="card-body">
+        <div class="row">
+          <div class="col-4">
+            <img src="${olkeAxtarilan[0].flags.png}" alt="" class="img-fluid" />
+          </div>
+          <div class="col8">
+            <h3 class="card-tittle">${olkeAxtarilan[0].name.common}</h3>
+            <hr />
+            <div class="row">
+              <div class="col-4">Əhali Sayı:</div>
+              <div class="col-8">${(
+                olkeAxtarilan[0].population / 1000000
+              ).toFixed(1)} milyon</div>
+            </div>
+            <div class="row">
+              <div class="col-4">Rəsmi Dil:</div>
+              <div class="col-8">${Object.values(
+                olkeAxtarilan[0].languages
+              )}</div>
+              </div>
+            <div class="row">
+              <div class="col-4">Paytaxt:</div>
+              <div class="col-8">${olkeAxtarilan[0].capital}</div>
+            </div>
+
+        </div>
+        `;
+
+        document
+          .querySelector("#countryNeigDetail")
+          .insertAdjacentHTML("beforeend", html);
+      });
+    }
   });
 }
-
-function olke(data) {
-  // console.log(data.name);
-  let html = `
-          
-          <div class="card-body">
-            <div class="row">
-              <div class="col-4">
-                <img src="${data.flags.png}" alt="" class="img-fluid" />
-              </div>
-              <div class="col8">
-                <h3 class="card-tittle">${data.name}</h3>
-                <hr />
-                <div class="row">
-                  <div class="col-4">Əhali Sayı:</div>
-                  <div class="col-8">${1}</div>
-                </div>
-                <div class="row">
-                  <div class="col-4">Rəsmi Dil:</div>
-                  <div class="col-8">zczxzx</div>
-                </div>
-                <div class="row">
-                  <div class="col-4">Paytaxt:</div>
-                  <div class="col-8">zczxzx</div>
-                </div>
-         
-            </div>
-          `;
-  document.querySelector("#countryDetail").innerHTML = html;
-}
-
-function qonsuOlkeler(data) {}
