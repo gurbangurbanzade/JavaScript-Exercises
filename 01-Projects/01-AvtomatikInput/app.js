068d22ac7d443d4112151f8145622ba33b9a325c
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-07-20 10:01:37 +0400

    Avtomatik Input Alqoritmi

--- 01-Projects/01-AvtomatikInput/app.js
+++ 01-Projects/01-AvtomatikInput/app.js
@@ -1,13 +1,27 @@
 function myFunction() {
   var x = document.getElementById("fname");
+
   if (x.value.length == 1) {
     x.value = x.value[0];
   } else if (x.value.length == 2) {
-    // x.value = x.value[0] + x.value[1];
-  } else if (x.value.length == 3 && x.value[3]) {
-    x.value = x.value[0] + x.value[1] + "/";
+    if (x.value[1] == "/") {
+      // x.value = x.value[0] + x.value[1];
+    } else if (x.value[0] == "/") {
+      x.value = x.value[1];
+    } else {
+      x.value = x.value[0] + x.value[1] + "/";
+    }
+  } else if (x.value.length == 3) {
+    if (x.value[3] == "/") {
+      x.value = x.value[0] + x.value[1] + "/" + x.value[2];
+    } else {
+      x.value = x.value[0] + x.value[1] + "/";
+    }
   } else if (x.value.length == 4 && x.value.length < 5) {
-    x.value = x.value[0] + x.value[1] + "/" + x.value[2];
+    if (x.value[2] == "/") {
+    } else {
+      x.value = x.value[0] + x.value[1] + "/" + x.value[2];
+    }
   } else if (x.value.length == 5 && x.value.length < 6) {
     x.value = x.value[0] + x.value[1] + "/" + x.value[3] + x.value[4];
   } else if (x.value.length >= 6) {
